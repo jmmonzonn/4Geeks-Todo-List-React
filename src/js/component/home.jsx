8c8d66367f3107fc7688265ffc7215b12b994823
jmmonzonn <jmmonzonn@gmail.com>
2022-03-04 07:55:03 +0000

    FINALIZADO! jmmonzonn

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,24 +1,76 @@
-import React from "react";
+import React, { useState } from "react";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
+	const [listOfTasks, setListOfTasks] = useState([]);
+	const [count, setCount] = useState(0);
+
+	const createTodo = (event) => {
+		if (event.key === "Enter") {
+			setListOfTasks([...listOfTasks, event.target.value]);
+			event.target.value = "";
+			setCount(count + 1);
+		}
+	};
+
+	const deleteTask = (index) => {
+		setListOfTasks(listOfTasks.filter((e, i) => index != i));
+		setCount(count - 1);
+	};
+
 	return (
-		<div>
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div className="container">
+			<div className="todoList">
+				<h3>Lista de Tareas</h3>
+				<p>by jmmonzonn</p>
+				<input
+					type="text"
+					placeholder="Introduce tarea"
+					onKeyDown={(e) => {
+						createTodo(e);
+					}}
+				/>
+				<div>
+					{listOfTasks.map((value, index) => {
+						return (
+							<div>
+								<div className="task" key={index}>
+									<div className="textTask">
+										{value}
+										<button
+											className="buttonTask"
+											onClick={() => {
+												deleteTask(index);
+											}}>
+											<svg
+												xmlns="http://www.w3.org/2000/svg"
+												width="16"
+												height="16"
+												fill="currentColor"
+												class="bi bi-trash3"
+												viewBox="0 0 16 16">
+												<path d="M6.5 1h3a.5.5 0 0 1 .5.5v1H6v-1a.5.5 0 0 1 .5-.5ZM11 2.5v-1A1.5 1.5 0 0 0 9.5 0h-3A1.5 1.5 0 0 0 5 1.5v1H2.506a.58.58 0 0 0-.01 0H1.5a.5.5 0 0 0 0 1h.538l.853 10.66A2 2 0 0 0 4.885 16h6.23a2 2 0 0 0 1.994-1.84l.853-10.66h.538a.5.5 0 0 0 0-1h-.995a.59.59 0 0 0-.01 0H11Zm1.958 1-.846 10.58a1 1 0 0 1-.997.92h-6.23a1 1 0 0 1-.997-.92L3.042 3.5h9.916Zm-7.487 1a.5.5 0 0 1 .528.47l.5 8.5a.5.5 0 0 1-.998.06L5 5.03a.5.5 0 0 1 .47-.53Zm5.058 0a.5.5 0 0 1 .47.53l-.5 8.5a.5.5 0 1 1-.998-.06l.5-8.5a.5.5 0 0 1 .528-.47ZM8 4.5a.5.5 0 0 1 .5.5v8.5a.5.5 0 0 1-1 0V5a.5.5 0 0 1 .5-.5Z" />
+											</svg>
+										</button>
+									</div>
+								</div>
+							</div>
+						);
+					})}
+				</div>
+			</div>
+			<div className="counter">
+				{count === 0 ? (
+					<h6>No hay tareas por hacer</h6>
+				) : count === 1 ? (
+					<h6>Te queda 1 tarea por hacer</h6>
+				) : (
+					<h6>Te quedan {count} tareas por hacer</h6>
+				)}
+			</div>
 		</div>
 	);
 };
